d94d5f7d56778fc4d54341253946477df8377542
tracer-ics2018 <tracer@njuics.org>
2019-05-08 09:06:20 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-47-generic #50~16.04.1-Ubuntu SMP Fri Mar 15 16:06:21 UTC 2019 x86_64 GNU/Linux  09:06:20 up  6:44,  2 users,  load average: 0.29, 0.30, 0.27 5e530d55502e8419d4c75cea164d9343b7aa7cbf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 
+#include <stdlib.h>
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
@@ -7,10 +8,16 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
+  TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-
+  TK_PLUS = '+', 
+  TK_SUB = '-',
+  TK_MULTI = '*',
+  TK_DIVIDE = '/',
+  TK_LP = '(',
+  TK_RP = ')', 
+  NUM
 };
 
 static struct rule {
@@ -22,9 +29,15 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {" +", TK_NOTYPE},  // spaces
+  {"\\+", '+'},       // plus
+  {"==", TK_EQ},      // equal
+  {"\\-", '-'},       // sub
+  {"\\*", '*'},       // multi
+  {"\\/", '/'},       // divide
+  {"[0-9]+", NUM},    // number
+  {"\\(", '('},       // left parentheses
+  {"\\)", ')'},       // right parentheses
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -80,7 +93,21 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE: break;
+		  case NUM:
+			/*
+			if(substr_len >= 32)
+			  substr_len = 31;*/
+			if(substr_len >=32) {
+			  printf("sub expr max len 31,your len: %d", substr_len);
+			  return false;
+			}
+			for(int i = 0;i < substr_len;i++) {
+			  tokens[nr_token].str[i] = substr_start[i];
+			}
+          default: 
+			tokens[nr_token].type = rules[i].token_type;
+			nr_token ++;
         }
 
         break;
@@ -96,6 +123,90 @@ static bool make_token(char *e) {
   return true;
 }
 
+static bool check_parentheses(int p, int q) {
+  int i, j = 0;
+  if(tokens[p].type != '(' || tokens[q].type != ')')
+	return false;
+  else {
+	for(i = p + 1;i < q;i ++){
+	  if(tokens[i].type == '(')
+		j ++;//push statck
+	  else if(tokens[i].type == ')') {
+		j --;//pop statck
+		if(j < 0)//statck low overflow
+		  return false;	//not matched
+	  }
+	}
+	if(j != 0)//statck not empty, not matched
+	  return false;
+	return true;
+  }
+}
+
+static inline bool is_op(int p) {
+  if(tokens[p].type == '+' || tokens[p].type == '-' || tokens[p].type == '*' || tokens[p].type == '/')
+	return true;
+  return false;
+}
+
+static int op_priority(Token t) {
+  int p;
+  switch(t.type) {
+	case '+':
+	case '-':p=0;break;
+	case '*':
+	case '/':p=1;break;
+	default: p = -1;
+  }
+  return p;
+}
+
+static int main_op_position(int p, int q) {
+  int i;
+  int poistion = p;
+  Token m_op=tokens[p];
+  for(i = p;i < q; i++) {
+	if(is_op(i)) {
+ 	  if(op_priority(tokens[i]) <= op_priority(m_op)) {
+	    m_op = tokens[i];
+		poistion = i;
+	  }
+	} 
+    else if(tokens[i].type == '(') {
+	  while(tokens[i].type != ')') {
+	    i ++;
+      }
+	}
+  }
+  return poistion;
+}
+
+static uint32_t eval(int p, int q) {
+  if(p > q) {
+	printf("Bad expression");
+	assert(0);
+  }
+  else if (p == q) {
+	return strtoul(tokens[p].str, NULL, 10);
+  }
+  else if (check_parentheses(p,q)) {
+	return eval(p+1, q-1);
+  }
+  else {
+	int op = main_op_position(p, q);
+	uint32_t val1 = eval(p, op-1);
+	uint32_t val2 = eval(op+1, q);
+
+	switch (tokens[op].type) {
+	  case '+': return val1 + val2;break;
+	  case '-': return val1 - val2;break;
+	  case '*': return val1 * val2;break;
+	  case '/': return val1 / val2;break;
+	  default: assert(0);
+	}
+  }
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -103,7 +214,5 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  return eval(0, nr_token-1);
 }
