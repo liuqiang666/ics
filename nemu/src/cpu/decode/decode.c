43abb300901409c46d540178eda6ad091afef4d5
tracer-ics2018 <tracer@njuics.org>
2019-05-13 13:05:20 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-47-generic #50~16.04.1-Ubuntu SMP Fri Mar 15 16:06:21 UTC 2019 x86_64 GNU/Linux  13:05:20 up  6:52,  2 users,  load average: 0.11, 0.16, 0.11 f38f23cba40fa132f9c4f521a6b3452d59bc41

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -41,7 +41,7 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm = instr_fetch(eip, op->width);
+  op->simm = (int32_t)instr_fetch(eip, op->width);
 
   rtl_li(&op->val, op->simm);
 
