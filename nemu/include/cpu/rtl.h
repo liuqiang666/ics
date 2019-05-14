63bbeffc49775f2780458de8ba1b677adf6d5a4a
liuqiang <1663863667@qq.com>
2019-05-14 08:27:01 +0000

    call

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -166,6 +166,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
+  printf("esp:%x", cpu.esp);
   rtl_subi(&cpu.esp, &cpu.esp, 4);
   // M[esp] <- src1
   rtl_sm(&cpu.esp, src1, 4);
