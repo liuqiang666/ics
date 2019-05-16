837a5036719aa204f9288f042accdc879d79b32e
tracer-ics2018 <tracer@njuics.org>
2019-05-16 13:35:04 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-47-generic #50~16.04.1-Ubuntu SMP Fri Mar 15 16:06:21 UTC 2019 x86_64 GNU/Linux  13:35:04 up 10:46,  3 users,  load average: 0.00, 0.05, 0.11 bab373bba285003dc7e3f16f49627e879cc2535d

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -205,11 +205,11 @@ make_DHelper(test_I) {
 }
 
 make_DHelper(SI2E) {
-  printf("before SI2E src: 0x%x\n", id_src->val);
   assert(id_dest->width == 2 || id_dest->width == 4);
   decode_op_rm(eip, id_dest, true, NULL, false);
   id_src->width = 1;
   decode_op_SI(eip, id_src, true);
+  printf("before SI2E src: 0x%x\n", id_src->val);
   if (id_dest->width == 2) {
     id_src->val &= 0xffff;
   }
