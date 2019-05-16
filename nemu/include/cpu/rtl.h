e019a6863df0436f3df2144236ed4c9bdbdb066c
tracer-ics2018 <tracer@njuics.org>
2019-05-16 13:56:29 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-47-generic #50~16.04.1-Ubuntu SMP Fri Mar 15 16:06:21 UTC 2019 x86_64 GNU/Linux  13:56:29 up 11:08,  3 users,  load average: 0.39, 0.28, 0.20 e1cc3a88c3a47fcdba5a6dbe41e3b7d465660e2f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -155,7 +155,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  printf("before sext,src1:0x%x\n", *src1);
+  printf("before sext,width: %d, src1:0x%x\n", width,  *src1);
   rtl_shli(&at, src1, 32 - width * 8);
   printf("after shili,src1:0x%x\n", at);
   switch(width) {
