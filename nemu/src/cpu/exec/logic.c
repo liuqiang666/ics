c2a6d41ae95dd42df9dc05fbad567f6cf0439878
tracer-ics2018 <tracer@njuics.org>
2019-05-19 11:29:04 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  11:29:04 up  7:38,  3 users,  load average: 0.46, 0.19, 0.11 d38b07a2f32ecb9e33c40bbf9afd387a2367d642

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -66,9 +66,12 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  
   print_asm_template2(shl);
 }
 
