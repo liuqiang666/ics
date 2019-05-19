c37582e78d4a74b96bef2725e7b59203bcb90b2f
tracer-ics2018 <tracer@njuics.org>
2019-05-19 11:25:16 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  11:25:16 up  7:34,  3 users,  load average: 0.14, 0.13, 0.09 331b13755ccbc82d441718d4c531333f2f5bc156

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -43,13 +43,11 @@ static make_EHelper(name) { \
 make_group(gp1,
     EX(add), EX(or), EX(adc), EMPTY,
     EX(and), EX(sub), EX(xor), EX(cmp))
-    //EX(add), EX(or), EX(adc), EX(sbb),
-    //EX(and), EX(sub), EX(xor), EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    EMPTY, EMPTY, EMPTY, EX(sar))
 
   /* 0xf6, 0xf7 */
 make_group(gp3,
