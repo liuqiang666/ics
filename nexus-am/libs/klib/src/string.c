ef743758a46c789bff1c148264e9ac46a81f1869
tracer-ics2018 <tracer@njuics.org>
2019-05-26 06:52:15 +0000

    >  run U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  06:52:15 up  2:28,  3 users,  load average: 0.16, 0.09, 0.10 f9a73c99e3b979a89e4c3b7f240cbf1f5b388b74

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -64,7 +64,7 @@ int strcmp(const char* s1, const char* s2) {
     return -1;
   if(s2 == NULL)
     return 1;
-  while(*s1 != '\0' && *s2 != '\0' && *s1 == *s2){
+  while(*s1 != '\0' && *s1 == *s2){
 	s1 ++;
     s2 ++;
   }
