e96788476dee0c8ad1d3eb04897b18697e28f635
tracer-ics2018 <tracer@njuics.org>
2019-05-27 13:10:30 +0000

    >  compile U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  13:10:30 up 10:23,  3 users,  load average: 0.46, 0.29, 0.19 fe708467743fb126e87f72c85183e1281f936edb

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,14 +3,18 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define VGA_PORT 0x100 //??
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
+	  //代码只模拟了400x300x32的图形模式
+      uint32_t screen = inl(VGA_PORT);
+      info->width = screen >> 16;//100
+      info->height = screen & 0xffff;//300
       return sizeof(_VideoInfoReg);
     }
   }
@@ -21,7 +25,9 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-
+ 	  int i;
+	  int size = screen_width() * screen_height();
+	  for (i = 0; i< size; i++) fb[i] = i;
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
