50db001cff87e9e380298b0893a7042a3f41bc99
tracer-ics2018 <tracer@njuics.org>
2019-06-01 16:21:20 +0000

    >  run U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  16:21:20 up 11:28,  1 user,  load average: 0.10, 0.09, 0.03 407fb6eef8e76408d1845c04d9392d04c420d499

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,15 @@ void sys_exit(int code) {
   _halt(code);
 }
 
+size_t sys_write(int fd, char *buf, size_t len) {
+  if(fd == 1 || fd == 2) {
+	for(int i = 0;i < len; i++)
+	  _putc(buf[i]);
+    return len;
+  }
+  return 0;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -21,6 +30,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_yield: c->GPR1 = sys_yield();break;
     case SYS_exit:Log("system exit."); sys_exit(a[1]);break;
+    case SYS_write: c->GPR1 = sys_write(a[1], (char *)a[2], a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   
