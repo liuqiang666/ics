3a31b05ff16011ccdedbfa01b40bfac80cf62e44
tracer-ics2018 <tracer@njuics.org>
2019-06-01 06:16:31 +0000

    >  run U201517080 liu Linux 6c04a62f8af6 4.15.0-50-generic #54~16.04.1-Ubuntu SMP Wed May 8 15:55:19 UTC 2019 x86_64 GNU/Linux  06:16:31 up  3:57,  1 user,  load average: 0.59, 0.39, 0.22 1bfad176e4bb41ed63ae5177741ecd57dd1040d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,24 @@
 #include "common.h"
 #include "syscall.h"
 
+
+uintptr_t sys_yield() {
+  _yield();
+  return 0;
+}
+
+
+
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
+    case SYS_yield: c->GPR1 = sys_yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
